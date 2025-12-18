69ebb0fa18169c945035ce33c1b2b868683a0a91
HelderDePegaso <115958521+HelderDePegaso@users.noreply.github.com>
2025-12-19 00:53:15 +0100

    feat: Retriving user data

--- src/app/entities/dashboard/dashboard/dashboard.component.ts
+++ src/app/entities/dashboard/dashboard/dashboard.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MenuComponent } from "../components/menu/menu.component";
 import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "../components/navbar/navbar.component";
+import { UserHttpclient } from '../../../core/http/user-httpclient/user-httpclient';
+import { UserContextService } from '../../../core/singleton/user.context.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -11,9 +13,23 @@ import { NavbarComponent } from "../components/navbar/navbar.component";
 })
 export class DashboardComponent implements OnInit {
 
+  userHttpClient = inject(UserHttpclient)
+  userContextService = inject(UserContextService)
+
   constructor(private router: Router , private activatedRoute: ActivatedRoute) { }
 
-  ngOnInit(): void {
+  async ngOnInit():   Promise<void> {
+    // Carregar dados necessários para o usuário e depois redirecinar para dash
+    const [ profile, myAreas, notifications ] = await Promise.all([
+      this.userHttpClient.profile(),
+      this.userHttpClient.getMyAreas(), 
+      this.userHttpClient.getNotifications()
+    ])
+
+    debugger
+    this.userContextService.setProfile(profile)
+    this.userContextService.setNotifications(notifications)
+    this.userContextService.setAreas(myAreas as any)
     this.router.navigate(['dash'], {relativeTo: this.activatedRoute})
   }
 }
