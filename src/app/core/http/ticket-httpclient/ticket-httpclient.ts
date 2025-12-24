52fcda2f166e3ea48a7e8b2b37a550dcebb6a389
HelderDePegaso <115958521+HelderDePegaso@users.noreply.github.com>
2025-12-24 08:01:29 +0100

    feat (ui/ux): Layout and request logic to create a new ticket

--- src/app/core/http/ticket-httpclient/ticket-httpclient.ts
+++ src/app/core/http/ticket-httpclient/ticket-httpclient.ts
@@ -1,2 +1,17 @@
+import { inject, Injectable } from "@angular/core";
+import { RootHttpClient } from "../httpClient-instance";
+import { firstValueFrom } from "rxjs";
+
+@Injectable({
+    providedIn   : 'root'
+})
 export class TicketHttpclient {
+
+    private http: RootHttpClient = inject(RootHttpClient) 
+
+
+    
+    createTicket(ticket: any) {
+        return firstValueFrom(this.http.makePost("tickets/create", ticket))
+    }
 }
--- src/app/features/ticket/components/ticket-new/ticket-new.component.ts
+++ src/app/features/ticket/components/ticket-new/ticket-new.component.ts
@@ -1,13 +1,113 @@
-import { Component } from '@angular/core';
+import { Component, inject, Renderer2 } from '@angular/core';
 import { HorizontalDragScrollDirective } from '../../../../core/directive/horizontal-drag-scroll-directive.directive';
+import { FormGroup, FormControl, ɵInternalFormsSharedModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AsyncPipe, NgFor, NgIf, TitleCasePipe } from '@angular/common';
+import { min } from 'rxjs';
+import { TicketHttpclient } from '../../../../core/http/ticket-httpclient/ticket-httpclient';
+import { UserContextService } from '../../../../core/singleton/user.context.service';
 
 @Component({
   selector: 'app-ticket-new',
   standalone: true,
-  imports: [HorizontalDragScrollDirective],
+  imports: [
+    HorizontalDragScrollDirective, 
+    ɵInternalFormsSharedModule , 
+    ReactiveFormsModule  ,  
+    NgFor ,
+    NgIf , 
+    AsyncPipe ,
+    TitleCasePipe
+  ],
   templateUrl: './ticket-new.component.html',
   styleUrl: './ticket-new.component.css'
 })
 export class TicketNewComponent {
 
+  ticketHttpClient = inject(TicketHttpclient);
+  userContextService = inject(UserContextService);
+
+  form = new FormGroup({
+    title: new FormControl('' , [Validators.required ,  Validators.minLength(6)]),
+    description: new FormControl('' , [Validators.required ]),
+    relatedDate: new FormControl(''),
+  })
+
+  areas$ = this.userContextService.areas$
+
+  selectedArea: {name: string, uuid: string}  |  null = null
+
+  constructor(private renderer: Renderer2) { }
+
+  ngOnInit(): void {
+    
+  }
+
+  cardSelected(cardId: string) {
+    this.deselectOldCard()
+    const card = this.renderer.selectRootElement(`#area-${cardId}` , true);
+
+
+    this.renderer.addClass(card, 'is-selected');
+  }
+
+
+  deselectCard(cardId: string) {
+    const card = this.renderer.selectRootElement(`#area-${cardId}` , true);
+    this.renderer.removeClass(card, 'is-selected');
+    this.selectedArea = null;
+  }
+
+  deselectOldCard() {
+    if  (!this.selectedArea) return
+    const oldCard = this.renderer.selectRootElement(`#area-${this.selectedArea?.uuid}` , true);
+    this.renderer.removeClass(oldCard, 'is-selected');
+  }
+
+  areaClick(area: any) {
+    
+    console.log(area);
+
+    this.cardSelected(area.uuid);
+    this.selectedArea = area
+  }
+
+  onInputFocus(event: FocusEvent) {
+    console.log(event);
+    (event.target as HTMLElement)?.parentElement?.classList.add('is-focused'); 
+  }
+
+  onInputFocusOut(event:  FocusEvent) {
+    console.log(event);
+    (event.target as HTMLElement)?.parentElement?.classList.remove('is-focused');
+  }
+
+
+  onInput() {
+    
+  }
+
+  onSubmit() {
+    if (this.form.invalid) return;
+    if (this.selectedArea === null) alert('Selecione uma area');
+    debugger
+    const data = {
+      ...this.form.value,
+      areaUuid: this.selectedArea?.uuid
+    };
+    
+    
+    this.ticketHttpClient.createTicket(data).then(() => { 
+      alert('Ticket criado com sucesso');
+    }).catch(() => {
+      alert('Erro ao criar ticket');
+    }).finally(() => {
+      this.form.reset();
+      this.deselectOldCard();
+      this.form.markAsPristine();
+      this.form.markAsUntouched();
+    })
+  }
+
+  
+  
 }
