b59411eb837e303f7dc8e7b0ff11c4369aeec923
HelderDePegaso <115958521+HelderDePegaso@users.noreply.github.com>
2025-12-28 14:25:12 +0100

    config: Angular Experimental Tan Stack Query installation and config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,16 +4,30 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { tokenInterceptor } from './core/interceptor/token.interceptor';
+import { provideTanStackQuery, QueryClient } from '@tanstack/angular-query-experimental';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)  ,
-    provideHttpClient( 
+    provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
+    provideHttpClient(
       withInterceptors([tokenInterceptor])
-     ), 
+    ),
 
-    { provide: LOCALE_ID, useValue: 'pt-PT' }
+    { provide: LOCALE_ID, useValue: 'pt-PT' },
+
+    provideTanStackQuery(new QueryClient({
+      defaultOptions: {
+        queries: {
+          staleTime: 5 * 60 * 1000,   // Áreas
+          //cacheTime: 10 * 60 * 1000,  // Áreas ficam na RAM 10 min após stale
+          refetchOnWindowFocus: true,
+          refetchOnReconnect: true,
+          retry: 1,
+          refetchInterval: false,      // só ativa polling específico quando necessário
+        },
+      },
+    }))
   ]
-  
-    
+
+
 };
